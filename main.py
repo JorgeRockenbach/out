118ad27cb74fcd9ee8494c62d7a86fc3e48f8b64
JorgeRockenbach <jorge.rockenbach@universo.univates.br>
2022-06-29 12:28:51 -0300

    Commit exemplo

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import streamlit as st;
 import Pages.Adicionar as adicionarDespesas;
 import Pages.List as listDespesas;
 
+#Flyway flyway = Flyway.configure().dataSource("jdbc:mysql://localhost:3306/out", "root", "root").load()
+#flyway.migrate() 
+
+
 st.sidebar.title('Menu')
 Page = st.sidebar.selectbox('Despesas', ['Adicionar', 'Consultar'])
 
